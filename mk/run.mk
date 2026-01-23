429a14762b803b50618c42fe7f4549a0d38491c5
Yeint <yeint_herp@proton.me>
2026-01-23 10:45:50 +0100

    Added a more faithful emulation

--- mk/run.mk
+++ mk/run.mk
@@ -22,17 +22,24 @@ define qemu_run_floppy
 		-drive "$$drive_opts" \
 		-m 64M \
 		-cpu "pentium-v1,mmx=on,fpu=on" \
-		-machine "pc-i440fx-7.2" \
 		-device cirrus-vga \
+		-audiodev pa,id=snd0 \
+		-device sb16,irq=11,dma=1,audiodev=snd0 \
+		-drive if=none,id=cd0,media=cdrom \
+		-device ide-cd,drive=cd0,bus=ide.1,unit=0,model="SONY DVD-ROM" \
 		-device "ne2k_pci,netdev=net0,addr=0x03" \
 		-netdev "user,id=net0,hostfwd=tcp::2222-:22" \
 		--no-reboot \
 		--no-shutdown \
 		-serial stdio \
 		-d "int,guest_errors" \
-		-M "accel=tcg,smm=off" \
+		-M "pc-i440fx-7.2,acpi=off,accel=tcg,smm=off" \
 		-D "$(QEMU_LOG)"
 endef
+
+# For MBR hard drives.
+# -drive file=hdd.img,if=none,id=hd0,format=raw
+# -device ide-hd,drive=hd0,bus=ide.0,unit=0
 
 define bochs_run_floppy
 	$(call require_tool,bochs)
